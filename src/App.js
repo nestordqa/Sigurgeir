3d18fd70cf52c1b8146d70d4933d6aa9b7902663
nestordqa <ndqa96@gmail.com>
2023-01-21 14:59:51 -0400

    React Router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import {Route, Switch} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import { Home } from './views/Home';
+import { Products } from './views/Products';
+import { AboutUs } from './views/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}>
+          </Route>
+          <Route exact path="/products" element={<Products/>}/>
+          <Route exact path="/aboutus" element={<AboutUs/>}/>
+        </Routes>
     </div>
   );
 }
