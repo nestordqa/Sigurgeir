dd99ce50667764c01fdab103ea63739f12bca6b1
nestordqa <ndqa96@gmail.com>
2023-01-21 14:46:25 -0400

    home view

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+export const Home = () => {
+  return (
+    <>
+        <Layout>
+            <div>
+                HOLA MUNDO HOME
+            </div>
+        </Layout>
+    </>
+  );
+};
