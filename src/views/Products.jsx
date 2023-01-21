5e27b4e9e74311c6334d2b1d558289cac42aeb36
nestordqa <ndqa96@gmail.com>
2023-01-21 15:01:00 -0400

    Products Page

---
+++ src/views/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+export const Products = () => {
+  return (
+    <>
+        <Layout>
+            <div>
+                Hola mundo desde Products
+            </div>
+        </Layout>  
+    </>
+  );
+};
