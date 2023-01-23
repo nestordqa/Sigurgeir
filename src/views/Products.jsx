ebdc4d12c85d5b7c8e0172d4222989b4615c6fd4
nestordqa <ndqa96@gmail.com>
2023-01-23 18:38:58 -0400

    cards products added

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import { useFetch } from '../customHooks/useFetch';
+import { Card } from '../components/Card.jsx/Card';
+import styles from '../styles/products.module.css';
 
 export const Products = () => {
+    const {data, isLoading, error} = useFetch('https://api.escuelajs.co/api/v1/products');
+
   return (
     <>
         <Layout>
-            <div>
-                Hola mundo desde Products
-            </div>
+          <div className={styles.container}>
+            {
+              isLoading ?
+              <div>
+                Loading content...
+              </div>
+              :
+              error ?
+              <div>
+                The was an error...
+              </div>
+              :
+              data?.map((product, index)=>{
+                return <Card 
+                key={product.title} 
+                title={product.title}
+                price={product.price}
+                images={product.images}
+                />
+              })
+            }
+          </div>
         </Layout>  
     </>
   );
